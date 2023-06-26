2d30d86be4a8bcfe4bb930bd13bc998ef6130d66
UPKAR CHAUDHARY <96167415+Upkar0904@users.noreply.github.com>
2023-06-27 01:20:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// Set the countdown date
+const countdownDate = new Date("2023-07-01T00:00:00Z").getTime();
+
+// Update the countdown every second
+const countdown = setInterval(function() {
+  const now = new Date().getTime();
+  const distance = countdownDate - now;
+
+  // Calculate days, hours, minutes, and seconds
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the countdown values
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+
+  // If the countdown is finished, display a message
+  if (distance < 0) {
+    clearInterval(countdown);
+    document.getElementById("days").innerText = "0";
+    document.getElementById("hours").innerText = "0";
+    document.getElementById("minutes").innerText = "0";
+    document.getElementById("seconds").innerText = "0";
+    alert("Countdown is over!");
+  }
+}, 1000);
